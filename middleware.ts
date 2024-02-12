0d5bbac76f044f665277709f2a960b7fa54c021a
smrsln <oasimarslan@gmail.com>
2024-02-12 21:44:30 +0300

    middleware relocated

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import { logger } from "@/utils/logger";
 
 function logRequest(req: NextRequest) {
   const serviceName = req.nextUrl.pathname.split("/")[2] || "unknown-service";
 
-  // Log the incoming request
-  logger.info(`API Request to ${req.nextUrl.pathname}`, {
+  console.info(`API Request to ${req.nextUrl.pathname}`, {
     method: req.method,
     path: req.nextUrl.pathname,
     userAgent: req.headers.get("user-agent"),
